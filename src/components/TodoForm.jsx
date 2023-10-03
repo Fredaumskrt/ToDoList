8676003364a8ca272fb78a1d3a436181d4b652ae
Frederico Malaquias <fmacaldeira@sga.pucminas.br>
2023-10-03 16:48:15 -0300

    test

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const TodoForm = () => {
+  return <div className="todo-form">
+    <h2>Criar Tarefa</h2>
+    <form>
+      <input type="text" placeholder="Digite o titulo"/>
+      <select>
+        <option value="">Selecione uma categoria</option>
+        <option value="Trabalho">Trabalho</option>
+        <option value="Pessoal">Pessoal</option>
+        <option value="Estudos">Estudos</option>
+      </select>
+      <button type="submit">Criar Tarefa</button>
+    </form>
+  </div>
+}
